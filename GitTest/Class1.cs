1f2a89bb9dada3484e2628aadf66bced4c2c779d
1102137216 <1102137216@gm.kuas.edu.tw>
2016-03-03 15:02:56 +0800

    hello

---
+++ GitTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitTest
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            System.Console.WriteLine("Hello~~~");
+            System.Console.ReadLine();
+        }
+    }
+}
